60ed99ef644598e3f83806ed08df994de29eaf92
Ricky.Pratama <pratamaricky@gmail.com>
2026-06-23 11:32:20 +0700

    berita baca

--- resources/views/depan/berita_baca.blade.php
+++ resources/views/depan/berita_baca.blade.php
@@ -30,7 +30,7 @@
                     <article class="post post-large blog-single-post border-0 m-0 p-0">
                         <div class="post-image ms-0">
                             <a href="blog-post.html">
-                                <img src="{{asset('uploads/berita/'.$berita->gambar)}}" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />
+                                <img src="{{asset('storage/berita/'.$berita->gambar)}}" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />
                             </a>
                         </div>
 
