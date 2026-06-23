4643c645e7515e4d2c014fd245de42f92a3907c8
Ricky.Pratama <pratamaricky@gmail.com>
2026-06-23 11:27:48 +0700

    index berita

--- resources/views/depan/berita.blade.php
+++ resources/views/depan/berita.blade.php
@@ -27,13 +27,12 @@
             {{-- KONTEN INTI --}}
             <div class="col-lg-9 order-lg-1">
                 <div class="blog-posts">
-                    @php
-                        if ($berita->count() == 0) {
-                            echo '<div class="alert alert-success">
-                                    Data yang Anda cari tidak ditemukan.
-                                </div>';
-                        }
-                    @endphp
+                    @if ($berita->count() == 0)
+                        <div class="alert alert-success">
+                            Data yang Anda cari tidak ditemukan.
+                        </div>
+                    @endif
+
                     @foreach ($berita as $brt)
                         <article class="post post-medium">
                             <div class="row mb-3">
@@ -41,11 +40,12 @@
                                     <div class="post-image">
                                         <a href="{{ url('/berita', $brt->slug) }}">
                                             @if ($brt->gambar != '')
-                                            <img src="{{asset('uploads/berita/'.$brt->gambar)}}" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />    
+                                                <img src="{{ asset('storage/berita/' . $brt->gambar) }}"
+                                                    class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" />
                                             @else
-                                            <img src="{{asset('uploads/logo/logoden-default.jpg')}}" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="">
+                                                <img src="{{ asset('storage/default-image/logoden-default.jpg') }}"
+                                                    class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="">
                                             @endif
-                                            {{-- <img src="{{asset('uploads/berita/'.$brt->gambar)}}" class="img-fluid img-thumbnail img-thumbnail-no-borders rounded-0" alt="" /> --}}
                                         </a>
                                     </div>
                                 </div>
@@ -55,10 +55,15 @@
                                             <a href="{{ url('/berita', $brt->slug) }}" class="tautanku1">{{ $brt->judul }}</a>
                                         </h2>
                                         <div class="post-meta lineheight-15">
-                                            <i class="far fa-calendar-alt"></i> <span>{{ App\Helpers\Gudangfungsi::tanggalindo_hari($brt->tanggal_publikasi) }} </span>
-                                            <span><a href="{{ url('/berita-kategori/'.$brt->kategori_slug) }}" class="tautanku2"><i class="far fa-folder"></i> {{ $brt->kategori_berita }}</a> </span>
+                                            <i class="far fa-calendar-alt"></i>
+                                            <span>{{ App\Helpers\Gudangfungsi::tanggalindo_hari($brt->tanggal_publikasi) }} </span>
+                                            <span>
+                                                <a href="{{ url('/berita-kategori/' . $brt->kategori_slug) }}" class="tautanku2">
+                                                    <i class="far fa-folder"></i> {{ $brt->kategori_berita }}
+                                                </a>
+                                            </span>
                                         </div>
-                                        @php echo substr($brt->isi_berita, 0, 290).' [...] '; @endphp
+                                        @php echo substr(strip_tags($brt->isi_berita), 0, 290) . ' [...]'; @endphp
                                     </div>
                                 </div>
                             </div>
@@ -68,7 +73,6 @@
                     <ul class="pagination float-end">
                         {{ $berita->links() }}
                     </ul>
-
                 </div>
             </div>
 
@@ -87,84 +91,100 @@
                         <h5 class="font-weight-semi-bold pt-4">Kategori</h5>
                         <ul class="nav nav-list flex-column mb-5">
                             @foreach ($kategori->get() as $kat)
-                                <li class="nav-item"><a class="nav-link" href="{{ url('/berita-kategori/'.$kat->kategori_slug) }}">{{ $kat->kategori_berita }}</a></li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/berita-kategori/' . $kat->kategori_slug) }}">{{ $kat->kategori_berita }}</a>
+                                </li>
                             @endforeach
-                        </ul>    
+                        </ul>
                     @endif
-                    
+
                     <h5>Infografis</h5>
-                    <div class="owl-carousel owl-theme nav-inside nav-inside-edge nav-squared nav-with-transparency nav-dark" data-plugin-options="{'items': 1, 'margin': 10, 'loop': false, 'nav': true, 'dots': false}">
+                    <div class="owl-carousel owl-theme nav-inside nav-inside-edge nav-squared nav-with-transparency nav-dark"
+                        data-plugin-options="{'items': 1, 'margin': 10, 'loop': false, 'nav': true, 'dots': false}">
                         @foreach ($infografis as $info)
-                        <div>
-                            {{-- <a class="img-thumbnail img-thumbnail-no-borders img-thumbnail-hover-icon" href="{{ asset('uploads/infografis/'.$info->gambar_sampul) }}"> --}}
-                            <a class="lightbox" href="{{ asset('uploads/infografis/'.$info->gambar_sampul) }}" data-plugin-options="{'type':'image'}">
-                                <img class="img-fluid rounded-0 mb-2 kotakku" src="{{ asset('uploads/infografis/'.$info->gambar_sampul) }}" alt="" />
-                            </a>
-                            <p class="text-2 mb-0 lineheight-17 ratatengah cetaktebal">{{ $info->judul_infografis }}</p>
-                        </div>
+                            <div>
+                                <a class="lightbox" href="{{ asset('storage/infografis/' . $info->gambar_sampul) }}"
+                                    data-plugin-options="{'type':'image'}">
+                                    <img class="img-fluid rounded-0 mb-2 kotakku"
+                                        src="{{ asset('storage/infografis/' . $info->gambar_sampul) }}" alt="" />
+                                </a>
+                                <p class="text-2 mb-0 lineheight-17 ratatengah cetaktebal">{{ $info->judul_infografis }}</p>
+                            </div>
                         @endforeach
                     </div>
 
                     <div class="tabs tabs-dark mb-4 pb-2">
                         <ul class="nav nav-tabs">
-                            <li class="nav-item"><a class="nav-link show active text-1 font-weight-bold text-uppercase" href="#popularPosts" data-bs-toggle="tab">Terpopuler</a></li>
-                            <li class="nav-item"><a class="nav-link text-1 font-weight-bold text-uppercase" href="#recentPosts" data-bs-toggle="tab">Terkini</a></li>
+                            <li class="nav-item">
+                                <a class="nav-link show active text-1 font-weight-bold text-uppercase"
+                                    href="#popularPosts" data-bs-toggle="tab">Terpopuler</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-1 font-weight-bold text-uppercase"
+                                    href="#recentPosts" data-bs-toggle="tab">Terkini</a>
+                            </li>
                         </ul>
                         <div class="tab-content">
+
+                            {{-- TERPOPULER --}}
                             <div class="tab-pane active" id="popularPosts">
                                 <ul class="simple-post-list">
                                     @foreach ($popularnews as $popnews)
-                                    <li>
-                                        <div class="post-image">
-                                            <div class="img-thumbnail img-thumbnail-no-borders d-block">
-                                                <a href="{{ url('/berita', $popnews->slug) }}">
-                                                    @if ($popnews->gambar != '')
-                                                    <img src="{{asset('uploads/berita/'.$popnews->gambar)}}" width="70" height="70" alt="">
-                                                    @else
-                                                    <img src="{{asset('uploads/logo/logoden-default-square.jpg')}}" width="70" height="70" alt="">
-                                                    @endif
-                                                </a>
+                                        <li>
+                                            <div class="post-image">
+                                                <div class="img-thumbnail img-thumbnail-no-borders d-block">
+                                                    <a href="{{ url('/berita', $popnews->slug) }}">
+                                                        @if ($popnews->gambar != '')
+                                                            <img src="{{ asset('storage/berita/' . $popnews->gambar) }}"
+                                                                width="70" height="70" style="object-fit:cover;" alt="">
+                                                        @else
+                                                            <img src="{{ asset('storage/default-image/logoden-default-square.jpg') }}"
+                                                                width="70" height="70" alt="">
+                                                        @endif
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="post-info fontsize-12 lineheight-17">
-                                            <a href="{{ url('/berita', $popnews->slug) }}" class="tautanku1">{{ $popnews->judul }}</a>
-                                            <div class="post-meta">
-                                                <span>
-                                                {{ App\Helpers\Gudangfungsi::tanggalindo_hari($popnews->tanggal_publikasi) }}
-                                                </span>
+                                            <div class="post-info fontsize-12 lineheight-17">
+                                                <a href="{{ url('/berita', $popnews->slug) }}" class="tautanku1">{{ $popnews->judul }}</a>
+                                                <div class="post-meta">
+                                                    <span>{{ App\Helpers\Gudangfungsi::tanggalindo_hari($popnews->tanggal_publikasi) }}</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
+                                        </li>
                                     @endforeach
                                 </ul>
                             </div>
+
+                            {{-- TERKINI --}}
                             <div class="tab-pane" id="recentPosts">
                                 <ul class="simple-post-list">
                                     @foreach ($recentnews as $recent)
-                                    <li>
-                                        <div class="post-image">
-                                            <div class="img-thumbnail img-thumbnail-no-borders d-block">
-                                                <a href="{{ url('/berita', $recent->slug) }}">
-                                                    @if ($recent->gambar != '')
-                                                    <img src="{{asset('uploads/berita/'.$recent->gambar)}}" width="70" height="70" alt="">
-                                                    @else
-                                                    <img src="{{asset('uploads/logo/logoden-default-square.jpg')}}" width="70" height="70" alt="">
-                                                    @endif
-                                                </a>
+                                        <li>
+                                            <div class="post-image">
+                                                <div class="img-thumbnail img-thumbnail-no-borders d-block">
+                                                    <a href="{{ url('/berita', $recent->slug) }}">
+                                                        @if ($recent->gambar != '')
+                                                            <img src="{{ asset('storage/berita/' . $recent->gambar) }}"
+                                                                width="70" height="70" style="object-fit:cover;" alt="">
+                                                        @else
+                                                            <img src="{{ asset('storage/default-image/logoden-default-square.jpg') }}"
+                                                                width="70" height="70" alt="">
+                                                        @endif
+                                                    </a>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="post-info fontsize-12 lineheight-17">
-                                            <a href="{{ url('/berita', $recent->slug) }}" class="fontsize-12">{{ $recent->judul }}</a>
-                                            <div class="post-meta">
-                                                <span>
-                                                    {{ App\Helpers\Gudangfungsi::tanggalindo_hari($popnews->tanggal_publikasi) }}
-                                                </span>
+                                            <div class="post-info fontsize-12 lineheight-17">
+                                                <a href="{{ url('/berita', $recent->slug) }}" class="fontsize-12">{{ $recent->judul }}</a>
+                                                <div class="post-meta">
+                                                    {{-- Fix: was incorrectly using $popnews->tanggal_publikasi --}}
+                                                    <span>{{ App\Helpers\Gudangfungsi::tanggalindo_hari($recent->tanggal_publikasi) }}</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </li>
+                                        </li>
                                     @endforeach
                                 </ul>
                             </div>
+
                         </div>
                     </div>
                 </aside>
@@ -177,7 +197,7 @@
 
 <script>
 $(document).ready(function() {
-    
+
 });
 </script>
 @endsection
