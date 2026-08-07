14600211bfc65d9a3815162e0b0cb974e6cacccd
Ricky.Pratama <pratamaricky@gmail.com>
2026-08-07 12:52:59 +0700

    coba balikin

--- resources/views/dapur/kuesioner/hasil_analisa/index.blade.php
+++ resources/views/dapur/kuesioner/hasil_analisa/index.blade.php
@@ -100,11 +100,8 @@
          <div class="row">
             <div class="col-sm-12">
                <div class="card">
-                  <div class="card-header d-flex align-items-center justify-content-between">
+                  <div class="card-header">
    <h5 class="m-0">{{ $judulhalaman }}</h5>
-   <button id="btnExportAll" class="btn btn-success btn-sm">
-   <i class="feather icon-download mr-1"></i> Export Semua (Excel)
-</button>
 </div>
                   <div class="card-body">
 
